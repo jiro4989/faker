e6de7c1dd5bf3978dbfa074d7a7adac7aa0015ca
jiro4989 <jiroron666@gmail.com>
2019-12-20 20:31:35 +0900

    macroでプロシージャを生成

--- src/fakerpkg/provider/address.nim
+++ src/fakerpkg/provider/address.nim
@@ -1,8 +1,18 @@
+import macros
+from strformat import `&`
+from strutils import join
 import ../base, address/[en_US, ja_JP]
 
-proc address*(f: Faker): string =
-  debugEcho f[]
-  case f.locale
-  of "en_US": en_US.address(f)
-  of "ja_JP": ja_JP.address(f)
-  else: en_US.address(f)
+macro genProc*(names, modules: untyped): untyped =
+  ## プロシージャ定義とその依存モジュールimport式を生成する。
+  var lines: seq[string]
+  for name in names:
+    lines.add(&"proc {name}*(f: Faker): string =")
+    lines.add(&"  case f.locale")
+    for m in modules:
+      lines.add(&"""  of "{repr(m)}": {repr(m)}.{repr(name)}(f)""")
+    lines.add(&"  else: en_US.{repr(name)}(f)")
+  parseStmt(lines.join("\n"))
+
+genProc [address, buildingNumber, city],
+        [en_US, ja_JP]
