4706068356c4bf8e74566178f2d3784572f897eb
jiro4989 <jiroron666@gmail.com>
2019-12-20 23:00:23 +0900

    :wip: 簡易CLIを追加

--- src/faker/cli/faker.nim
+++ src/faker/cli/faker.nim
@@ -1,3 +1,20 @@
+import os
+
 import ../../faker
 
-echo "test"
+proc main(args: seq[string]): int =
+  if args.len < 1:
+    stderr.writeLine "Need args"
+    return 1
+
+  let subcmd = args[0]
+  let fake = newFaker()
+  case subcmd
+  of "address":
+    echo fake.address()
+  else:
+    discard
+
+when isMainModule:
+  let args = commandLineParams()
+  quit main(args)
