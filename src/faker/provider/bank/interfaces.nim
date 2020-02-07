e5f5a26756102d8542afd4e0c8c0d4239fbc7036
jiro4989 <jiroron666@gmail.com>
2020-02-07 19:57:12 +0900

    :construction: generated bank module #8

---
+++ src/faker/provider/bank/interfaces.nim
@@ -0,0 +1,11 @@
+# interfaces はプロシージャの定義のみをまとめたモジュール。
+# C言語でいうヘッダファイルのように使用する。
+#
+# このモジュールをincludeしたモジュールは、このモジュールに定義しているプロシー
+# ジャを全て実装しないと、コンパイルが通らない。
+
+import random
+import ../../base
+export base
+
+proc job*(f: Faker): string
