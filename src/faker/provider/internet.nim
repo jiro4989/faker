176bd9379f52fad1fc310d293d9e35f09859f43b
jiro4989 <jiroron666@gmail.com>
2021-01-12 08:28:36 +0900

    update

--- src/faker/provider/internet.nim
+++ src/faker/provider/internet.nim
@@ -4,16 +4,65 @@
 # ----------------------------------------------- #
 
 import ../base
-import job/[job_en_US, job_ja_JP]
+import internet/[internet_en_US]
 export base
 
-proc job*(f: Faker): string =
-  ## Generates random job.
+proc safeDomainName*(f: Faker): string =
+  ## Generates random safeDomainName.
   runnableExamples:
     let f = newFaker()
-    echo f.job()
+    echo f.safeDomainName()
 
   case f.locale
-  of "en_US": job_en_US.job(f)
-  of "ja_JP": job_ja_JP.job(f)
-  else: job_en_US.job(f)
+  of "en_US": internet_en_US.safeDomainName(f)
+  else: internet_en_US.safeDomainName(f)
+
+proc freeDomainName*(f: Faker): string =
+  ## Generates random freeDomainName.
+  runnableExamples:
+    let f = newFaker()
+    echo f.freeDomainName()
+
+  case f.locale
+  of "en_US": internet_en_US.freeDomainName(f)
+  else: internet_en_US.freeDomainName(f)
+
+proc safeEmail*(f: Faker): string =
+  ## Generates random safeEmail.
+  runnableExamples:
+    let f = newFaker()
+    echo f.safeEmail()
+
+  case f.locale
+  of "en_US": internet_en_US.safeEmail(f)
+  else: internet_en_US.safeEmail(f)
+
+proc freeEmail*(f: Faker): string =
+  ## Generates random freeEmail.
+  runnableExamples:
+    let f = newFaker()
+    echo f.freeEmail()
+
+  case f.locale
+  of "en_US": internet_en_US.freeEmail(f)
+  else: internet_en_US.freeEmail(f)
+
+proc email*(f: Faker): string =
+  ## Generates random email.
+  runnableExamples:
+    let f = newFaker()
+    echo f.email()
+
+  case f.locale
+  of "en_US": internet_en_US.email(f)
+  else: internet_en_US.email(f)
+
+proc topLevelDomain*(f: Faker): string =
+  ## Generates random topLevelDomain.
+  runnableExamples:
+    let f = newFaker()
+    echo f.topLevelDomain()
+
+  case f.locale
+  of "en_US": internet_en_US.topLevelDomain(f)
+  else: internet_en_US.topLevelDomain(f)
--- src/faker/provider/internet/interfaces.nim
+++ src/faker/provider/internet/interfaces.nim
@@ -8,4 +8,9 @@ import random
 import ../../base
 export base
 
-proc job*(f: Faker): string
+proc safeDomainName*(f: Faker): string
+proc freeDomainName*(f: Faker): string
+proc safeEmail*(f: Faker): string
+proc freeEmail*(f: Faker): string
+proc email*(f: Faker): string
+proc topLevelDomain*(f: Faker): string
