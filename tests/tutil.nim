5e4b633af8194fb4776fe58f6e9302f548b70b29
jiro4989 <jiroron666@gmail.com>
2022-02-19 10:41:10 +0900

    Fix test

--- tests/tutil.nim
+++ tests/tutil.nim
@@ -1,20 +1,26 @@
-import unittest, random, times
+discard """
+  exitCode: 0
+  output: ""
+"""
+
+import std/unittest
+import std/random
+import std/times
 
 import faker/provider/util
 
-suite "proc formatNumbers":
-  setup:
-    let now = times.getTime()
-    var r = initRand(convert(Seconds, Nanoseconds, now.toUnix) + now.nanosecond)
-  test "Normal":
-    let tests = [
-      ("#####", 5),
-      ("###-###", 7),
-      ("###-###x###.####", 16),
-      ("+1-###-###", 10),
-      ]
-    for tt in tests:
-      for i in 1..100:
-        let (format, want) = tt
-        let got = r.formatNumbers(format)
-        check got.len == want
+block:
+  checkpoint "proc formatNumbers"
+  let now = times.getTime()
+  var r = initRand(convert(Seconds, Nanoseconds, now.toUnix) + now.nanosecond)
+  let tests = [
+    ("#####", 5),
+    ("###-###", 7),
+    ("###-###x###.####", 16),
+    ("+1-###-###", 10),
+    ]
+  for tt in tests:
+    for i in 1..100:
+      let (format, want) = tt
+      let got = r.formatNumbers(format)
+      check got.len == want
