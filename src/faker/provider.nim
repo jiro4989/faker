52c07327fd5e01e479385e41a2315e545fce7b09
jiro4989 <jiroron666@gmail.com>
2022-02-19 10:21:36 +0900

    import文を修正

--- src/faker/provider.nim
+++ src/faker/provider.nim
@@ -3,5 +3,32 @@
 # See 'tools/generator'.                          #
 # ----------------------------------------------- #
 
-import provider/[address, automotive, bank, company, currency, file, internet, isbn, job, lorem, misc, person, phone_number, user_agent]
-export address, automotive, bank, company, currency, file, internet, isbn, job, lorem, misc, person, phone_number, user_agent
+import provider/address
+import provider/automotive
+import provider/bank
+import provider/company
+import provider/currency
+import provider/file
+import provider/internet
+import provider/isbn
+import provider/job
+import provider/lorem
+import provider/misc
+import provider/person
+import provider/phone_number
+import provider/user_agent
+
+export address
+export automotive
+export bank
+export company
+export currency
+export file
+export internet
+export isbn
+export job
+export lorem
+export misc
+export person
+export phone_number
+export user_agent
